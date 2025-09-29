5e4b16bd268eabcf305f2c59e67a13f6d055adb1
Marcus Edel <marcus.edel@collabora.com>
2025-09-29 18:44:14 -0400

    Ignore GGML_OP_SET_ROWS parameters during gradient calculation, since there is no effect on the output gradients.
    Signed-off-by: Marcus Edel <marcus.edel@collabora.com>

--- src/llama-context.cpp
+++ src/llama-context.cpp
@@ -1390,7 +1390,7 @@ uint32_t llama_context::graph_max_nodes() const {
     if (model.arch == LLM_ARCH_QWEN3NEXT) {
         return std::max<uint32_t>(8192u, 32u*model.n_tensors());
     }
-    return std::max<uint32_t>(1024u, 8u*model.n_tensors());
+    return std::max<uint32_t>(2048u, 32u*model.n_tensors());
 }
 
 llm_graph_result * llama_context::get_gf_res_reserve() const {
