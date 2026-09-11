54db4c10963950655c6d117b79d915e1f9be7409
amangupta-tether <aman.gupta@tether.io>
2026-09-11 13:37:24 +0530

    QVAC-24124: set tensor 2d hash (#215)

--- tests/test-rpc.cpp
+++ tests/test-rpc.cpp
@@ -237,6 +237,47 @@ int main() {
         }
     }
 
+    {
+        ggml_context_ptr ctx_cached{ ggml_init(params) };
+        if (ctx_cached == nullptr) {
+            return 1;
+        }
+        ggml_tensor * tensor_cached = ggml_new_tensor_2d(ctx_cached.get(), GGML_TYPE_F32, 1024 * 1024, 3);
+        ggml_backend_buffer_ptr buffer_cached(ggml_backend_alloc_ctx_tensors(ctx_cached.get(), backend_a.get()));
+        if (buffer_cached == nullptr) {
+            return 1;
+        }
+
+        const size_t cached_row_size = tensor_cached->nb[1];
+        const size_t cached_rows = tensor_cached->ne[1];
+        const size_t cached_input_stride = cached_row_size + 2 * sizeof(float);
+        const size_t cached_output_stride = cached_row_size + 3 * sizeof(float);
+        std::vector<uint8_t> cached_input(cached_input_stride * cached_rows);
+        std::vector<uint8_t> cached_output(cached_output_stride * cached_rows, 0);
+        for (size_t row = 0; row < cached_rows; row++) {
+            for (size_t i = 0; i < cached_row_size; i++) {
+                cached_input[row * cached_input_stride + i] = (uint8_t) (row + i);
+            }
+        }
+
+        ggml_backend_tensor_set_2d_async(backend_a.get(), tensor_cached, cached_input.data(), 0, cached_row_size,
+                                         cached_rows, tensor_cached->nb[1], cached_input_stride);
+        ggml_backend_synchronize(backend_a.get());
+        ggml_backend_tensor_memset(tensor_cached, 0, 0, ggml_nbytes(tensor_cached));
+        ggml_backend_tensor_set_2d_async(backend_a.get(), tensor_cached, cached_input.data(), 0, cached_row_size,
+                                         cached_rows, tensor_cached->nb[1], cached_input_stride);
+        ggml_backend_tensor_get_2d_async(backend_a.get(), tensor_cached, cached_output.data(), 0, cached_row_size,
+                                         cached_rows, tensor_cached->nb[1], cached_output_stride);
+        ggml_backend_synchronize(backend_a.get());
+        for (size_t row = 0; row < cached_rows; row++) {
+            if (memcmp(cached_input.data() + row * cached_input_stride,
+                       cached_output.data() + row * cached_output_stride, cached_row_size) != 0) {
+                fprintf(stderr, "cached 2D RPC transfer mismatch\n");
+                return 1;
+            }
+        }
+    }
+
     ggml_backend_event_t events[4] = {
         ggml_backend_event_new(device_a),
         ggml_backend_event_new(device_a),
